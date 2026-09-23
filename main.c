0c9627b741ec6f5b57f46ac74391b83584465f4a
Felipe Falqueto Vicente <n7diedie@gmail.com>
2026-09-23 01:00:02 +0000

    feat: implementa modulo de pagamento a prazo com juros de 8%

--- main.c
+++ main.c
@@ -6,8 +6,13 @@ void exibir_menu(void) {
     printf("\n=== CONTROLE DE ESTOQUE ===\n");
     printf("1 - Listar produtos\n");
     printf("2 - Exibir valor total em estoque\n");
+    printf("4 - Exibir total a prazo (com juros)\n");
     printf("0 - Sair\n");
     printf("Escolha uma opcao: ");
+}
+
+float aplicar_juros(float total) {
+    return total + (total * TAXA_JUROS);
 }
 
 void listar_produtos(Produto lista[], int total) {
@@ -54,6 +59,9 @@ int main(void) {
                 break;
             case 2:
                 printf("\nTotal em estoque: R$ %.2f\n", calcular_total(estoque, total_produtos));
+                break;
+            case 4:
+                printf("\nTotal a prazo com juros: R$ %.2f\n", aplicar_juros(calcular_total(estoque, total_produtos)));
                 break;
             case 0:
                 printf("\nEncerrando o programa...\n");
